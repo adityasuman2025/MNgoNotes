c259d2ac111c53048b6c70a6ea5bcbe2b5df57d8
adityasuman2025 <adityasuman2025@gmail.com>
2020-10-18 12:31:41 +0530

    first tym loading list from interent done in home screen

--- screen/Home.js
+++ screen/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, AsyncStorage, Image, ActivityIndicator, BackAndroid } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, AsyncStorage, Image, ActivityIndicator } from 'react-native';
 import axios from 'axios';
-import {Actions} from 'react-native-router-flux';
+import { Actions } from 'react-native-router-flux';
 
 import Header from '../components/header';
 import { toast } from '../components/toast';
@@ -9,275 +9,231 @@ import { api_url_address } from "../constants";
 
 import { globalStyles } from '../styles/globalStyles';
 
-export default function Home({toCarry}) 
-{
-  const logged_user_id = toCarry.logged_user_id;
+export default function Home({ toCarry }) {
+    const logged_user_id = toCarry.logged_user_id;
 
-  const [notes, setNotes] = useState([]);
-  const [showIndicator, setShowIndicator] = useState(true);
+    const [notes, setNotes] = useState([]);
+    const [showIndicator, setShowIndicator] = useState(true);
 
-  const [counter, setCounter] = useState(1);
+    //componentDidMount
+    useEffect(() => {
+        //here we first fetch data(list) from API and store fetched data in cookies
+        //and then update state from that cookie
+        fetchListFromAPI(); //fetching list from cookie
+    }, []);
 
-//function to refresh the list
-  const refreshList = () =>
-  {
-    console.log("list refreshed");
-
-    setCounter(counter + 1);
-  }
-
-//here it updates UI on change of counter
-  useEffect(() => //useEffect works as componentDidMount
-  {
-    if(logged_user_id == "")
-    {
-      toast("you are not logged in");
+    //function to refresh the list
+    const refreshList = () => {
+        fetchListFromAPI();
     }
-    else
-    {
-      var user_id = logged_user_id;       
-      var user_notes_of = "user_notes_of_" + user_id;
 
-    //looking for user notes data in cookies  
-      AsyncStorage.getItem(user_notes_of).then((val) =>
-      {
-        if(val != null) //if some data exist in cookies then loading in flatlist
-        {
-          console.log("list loaded from cookie");
-          var data = JSON.parse(val);          
-          setNotes(data);
-          
-          if(showIndicator == true)
-            setShowIndicator(false); //hiding loading animation
+    //function to update state (list of notes) from cookies
+    async function loadListFromCookies() {
+        try {
+            //looking for user notes data in cookies
+            const user_id = logged_user_id;
+            const user_notes_of = "user_notes_of_" + user_id;
+
+            AsyncStorage.getItem(user_notes_of).then((val) => {
+                if (val != null) {
+                    //if some data exist in cookies then loading in flatlist
+                    console.log("list loaded from cookie");
+                    const data = JSON.parse(val);
+                    setNotes(data);
+                } else {
+                    toast("something went wrong");
+                }
+            });
+        } catch {
+            toast("something went wrong");
         }
-        else
-        {
-        //posting request to API for getting user notes data from server and storing it as cookies
-          const api_end_point = api_url_address + "getUserNotes.php";
-          axios.post( api_end_point, {
-            user_id: user_id
-          })
-          .then(function(response) 
-          {
-            try
-            {
-              var dataString = JSON.stringify((response.data));
-              console.log("list loaded from internet");
 
-              if(dataString == 0)
-              {
+        setShowIndicator(false); //hiding loading animation
+    }
+
+    //function to fetch data form api
+    async function fetchListFromAPI() {
+        try {
+            const api_end_point = api_url_address + "getUserNotes.php";
+            const response = await axios.post(api_end_point, {
+                user_id: logged_user_id
+            });
+            const dataString = JSON.stringify(response.data);
+            console.log("list fetched from internet");
+
+            if (dataString == 0) {
                 toast("failed to fetch data");
-              }
-              else if(dataString == -1)
-              {
+            } else if (dataString == -1) {
                 toast("something went wrong");
-              }
-              else //some data is there
-              {
-                if(showIndicator == true)
-                  setShowIndicator(false);//hiding loading animation
-
-              //making cookie of the list of notes  
-                AsyncStorage.setItem(user_notes_of, dataString);
-                Actions.refresh();
-
-              //refreshing for loading UI  
-                refreshList();
-              }
+            } else {
+                //some data is there
+                //making cookie of the list of notes
+                const user_id = logged_user_id;
+                const user_notes_of = "user_notes_of_" + user_id;
+                await AsyncStorage.setItem(user_notes_of, dataString);
             }
-            catch
-            {
-              toast("failed to get your updated data");
-            }          
-          })
-          .catch(error => 
-          {
+        } catch {
             toast("please check your internet connection");
-          });
-        }  
-      });
+        }
+
+        loadListFromCookies(); //loading list from cookie
     }
-  }, [counter]);
 
-//on clicking on any notes
-  const onClickingOnAnyNotes = (item) =>
-  {
-    setShowIndicator(true);
-    
-    // refreshList();
+    //on clicking on any notes
+    const onClickingOnAnyNotes = (item) => {
+        setShowIndicator(true);
 
-    var toCarry = {};
-    toCarry['logged_user_id'] = logged_user_id;
-    toCarry['notes_id'] = item.notes_id;
-    toCarry['title'] = item.title;
-    toCarry['type'] = item.type;    
+        var toCarry = {};
+        toCarry['logged_user_id'] = logged_user_id;
+        toCarry['notes_id'] = item.notes_id;
+        toCarry['title'] = item.title;
+        toCarry['type'] = item.type;
 
-  //loading notes list data from internet
-    const api_end_point = api_url_address + "getListDataOfANote.php";
-    axios.post( api_end_point, {
-      notes_id: item.notes_id
-    })
-    .then(function(response) 
-    {
-      setShowIndicator(false);
+        //loading notes list data from internet
+        const api_end_point = api_url_address + "getListDataOfANote.php";
+        axios.post(api_end_point, {
+            notes_id: item.notes_id
+        })
+            .then(function(response) {
+                setShowIndicator(false);
 
-      var data = response.data;
-      var dataString = JSON.stringify((response.data));
+                var data = response.data;
+                var dataString = JSON.stringify((response.data));
 
-      if(dataString == 0)
-      {
-        toast("failed to fetch data");
-      }
-      else if(dataString == -1)
-      {
-        toast("something went wrong");
-      }
-      else //some data is there
-      {
-        try
-        {
-        //making cookie of that notes_id list
-          var user_notes_list_data = logged_user_id + "_list_data_for_notes_id_" + item.notes_id;
-          AsyncStorage.setItem(user_notes_list_data, dataString);
+                if (dataString == 0) {
+                    toast("failed to fetch data");
+                } else if (dataString == -1) {
+                    toast("something went wrong");
+                } else {
+                    //some data is there
+                    try {
+                        //making cookie of that notes_id list
+                        var user_notes_list_data = logged_user_id + "_list_data_for_notes_id_" + item.notes_id;
+                        AsyncStorage.setItem(user_notes_list_data, dataString);
 
-        //redirecting to notes view page  
-          toCarry['notesOldList'] = dataString;
-          Actions.ViewNotesPage({ toCarry: toCarry, refreshList: () => refreshList()  }); //transfering refreshList function so that notes list can be refreshed when a note is deleted
-        }
-        catch(error)
-        {
-          toast("failed to get your updated data");
-        } 
-      }
-    })
-    .catch(error => 
-    {
-    //looking for user notes list in cookies if internet not available
-      var user_notes_list_data = logged_user_id + "_list_data_for_notes_id_" + item.notes_id;
-      AsyncStorage.getItem(user_notes_list_data).then((val) =>
-      {
-        setShowIndicator(false);
+                        //redirecting to notes view page
+                        toCarry['notesOldList'] = dataString;
+                        Actions.ViewNotesPage({ toCarry: toCarry, refreshList: () => refreshList() }); //transfering refreshList function so that notes list can be refreshed when a note is deleted
+                    } catch (error) {
+                        toast("failed to get your updated data");
+                    }
+                }
+            })
+            .catch(error => {
+                //looking for user notes list in cookies if internet not available
+                var user_notes_list_data = logged_user_id + "_list_data_for_notes_id_" + item.notes_id;
+                AsyncStorage.getItem(user_notes_list_data).then((val) => {
+                    setShowIndicator(false);
 
-        if(val != null) //if some data exist in cookies then loading in flatlist
-        {
-          toCarry['notesOldList'] = val;
-          Actions.ViewNotesPage({ toCarry: toCarry, refreshList: () => refreshList()  }); //transfering refreshList function so that list of notes can be refreshed when a note is deleted
-        }
-        else
-        {
-          toast("please check your internet connection");
-        }
-      });  
-    }); 
-  }
+                    if (val != null) {
+                        //if some data exist in cookies then loading in flatlist
+                        toCarry['notesOldList'] = val;
+                        Actions.ViewNotesPage({ toCarry: toCarry, refreshList: () => refreshList() }); //transfering refreshList function so that list of notes can be refreshed when a note is deleted
+                    } else {
+                        toast("please check your internet connection");
+                    }
+                });
+            });
+    }
 
-//on clicking on +/add item btn  
-  const createNewNoteBtnClickHandler = () =>
-  {
-    var toCarry = {};
-    toCarry['logged_user_id'] = logged_user_id;
-    
-    // console.log("new notes is going to be created");
-    Actions.createNotesForm({ toCarry: toCarry, refreshList: () => refreshList()  }); //transfering refreshList function so that list of notes can be refreshed when a new notes is created
-  }
+    //on clicking on +/add item btn
+    const createNewNoteBtnClickHandler = () => {
+        var toCarry = {};
+        toCarry['logged_user_id'] = logged_user_id;
 
-//rendering
-  return (
-    <View style={globalStyles.container}>
-      <Header toCarry={ {title: "MNgo Notes"} } />
-      {
-        showIndicator ?
-          <ActivityIndicator size="large" color="#d8d8d8" />
-          : null
-      }
-      <FlatList 
-        style = {styles.list}
-        data={notes}
-        keyExtractor ={(item) => item.notes_id }       
-        renderItem = {({item}) => (
-          <TouchableOpacity style={styles.box} onPress={() => onClickingOnAnyNotes(item)}>
-            <Image 
-                source={item.type == 1 ? require('../img/notes_icon.png'): require('../img/todos_icon.png')} 
-                style={styles.icon} 
-              />
-            <View>
-              <Text style={styles.listText} >{ item.title }</Text>
-              <Text style={styles.listType} >{ item.type == 1 ? "text" : "checkbox" } </Text>
-            </View>            
-          </TouchableOpacity>
-        )}
-      />
+        Actions.createNotesForm({ toCarry: toCarry, refreshList: () => refreshList() }); //transfering refreshList function so that list of notes can be refreshed when a new notes is created
+    }
 
-      <TouchableOpacity style={styles.addNotesBtn} onPress={() => createNewNoteBtnClickHandler()}>
-        <Image source={require('../img/add1.png')} style={styles.titleImg} />
-      </TouchableOpacity>
-    </View>
-  );
+    //rendering
+    return (
+        <View style={globalStyles.container}>
+            <Header toCarry={{ title: "MNgo Notes" }} />
+            {
+                showIndicator ?
+                    <ActivityIndicator size="large" color="#d8d8d8" />
+                    : null
+            }
+            <FlatList
+                style={styles.list}
+                data={notes}
+                keyExtractor={(item) => item.notes_id}
+                renderItem={({ item }) => (
+                    <TouchableOpacity style={styles.box} onPress={() => onClickingOnAnyNotes(item)}>
+                        <Image
+                            source={item.type == 1 ? require('../img/notes_icon.png') : require('../img/todos_icon.png')}
+                            style={styles.icon}
+                        />
+                        <View>
+                            <Text style={styles.listText} >{item.title}</Text>
+                            <Text style={styles.listType} >{item.type == 1 ? "text" : "checkbox"} </Text>
+                        </View>
+                    </TouchableOpacity>
+                )}
+            />
+
+            <TouchableOpacity style={styles.addNotesBtn} onPress={() => createNewNoteBtnClickHandler()}>
+                <Image source={require('../img/add1.png')} style={styles.titleImg} />
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  addNotesBtn:
-  {
-    position: 'absolute',
-    bottom: 8,
-    right:  8,
-    height: 50,
-    width:  50,
-    borderRadius: 100,
-    backgroundColor: "#181915", //455a64
-    alignItems:'center',
-    justifyContent :'center'
-  },
+    addNotesBtn: {
+        position: 'absolute',
+        bottom: 8,
+        right: 8,
+        height: 50,
+        width: 50,
+        borderRadius: 100,
+        backgroundColor: "#181915", //455a64
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
 
-  titleImg:
-  {
-    height: '50%',
-    width: '50%',
-    tintColor: '#455a64',
-  },
+    titleImg: {
+        height: '50%',
+        width: '50%',
+        tintColor: '#455a64',
+    },
 
-  list:
-  {
-    width: '100%',
-    height: '110%',
-  },
+    list: {
+        width: '100%',
+        height: '110%',
+    },
 
-  box:
-  {
-    borderBottomColor: '#3d4e56',
-    borderBottomWidth: 1,
+    box: {
+        borderBottomColor: '#3d4e56',
+        borderBottomWidth: 1,
 
-    padding: 15,
-    flexDirection: 'row',
-    marginHorizontal: 5,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
+        padding: 15,
+        flexDirection: 'row',
+        marginHorizontal: 5,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
 
-  icon:
-  {
-    height: 75,
-    width: 60,
-    tintColor: '#fff',
-    marginRight: 10,
-  },
+    icon: {
+        height: 75,
+        width: 60,
+        tintColor: '#fff',
+        marginRight: 10,
+    },
 
-  listText:
-  {
-    color: "#d8d8d8",
-    fontWeight: 'bold',
-    fontSize: 16,
-    maxWidth: '83%',
-    minWidth: '83%',
-  },
+    listText: {
+        color: "#d8d8d8",
+        fontWeight: 'bold',
+        fontSize: 16,
+        maxWidth: '83%',
+        minWidth: '83%',
+    },
 
-  listType:
-  {
-    color: "#1c313a",
-    textAlign: 'left',
-    padding: 0,
-    margin :0,
-    width: '100%',
-    fontSize: 13,
-  },  
+    listType: {
+        color: "#1c313a",
+        textAlign: 'left',
+        padding: 0,
+        margin: 0,
+        width: '100%',
+        fontSize: 13,
+    },
 });
--- screen/PassCode.js
+++ screen/PassCode.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, AsyncStorage, ActivityIndicator } from 'react-native';
 import axios from 'axios';
-import {Actions} from 'react-native-router-flux';
+import { Actions } from 'react-native-router-flux';
 
 import Header from '../components/header';
 import { toast } from '../components/toast';
@@ -9,40 +9,39 @@ import { api_url_address } from "../constants";
 
 import { globalStyles } from '../styles/globalStyles';
 
-export default function PassCode({toCarry}) 
-{
-  const logged_user_id = toCarry.logged_user_id;
+export default function PassCode({ toCarry }) {
+    const logged_user_id = toCarry.logged_user_id;
 
-  const [showIndicator, setShowIndicator] = useState(false);
-  const [error, setError] = useState("");
-  const [enteredPassCode, setEnteredPassCode] = useState("");
+    const [showIndicator, setShowIndicator] = useState(false);
+    const [error, setError] = useState("");
+    const [enteredPassCode, setEnteredPassCode] = useState("");
 
-//when pass code is entered
+    //when pass code is entered
     useEffect(() => {
-    //verifying entered pass cpde
+        //verifying entered pass cpde
         const enteredPassCodeLength = enteredPassCode.length;
-        if( enteredPassCodeLength === 4 ) {
-        //if entered pass code is 4 digits long
+        if (enteredPassCodeLength === 4) {
+            //if entered pass code is 4 digits long
             setShowIndicator(true); //displaying loading animation
 
-        //checking if someone is logged or not
-            if(logged_user_id === "") {
+            //checking if someone is logged or not
+            if (logged_user_id === "") {
                 toast("you are not logged in");
             } else {
-                var user_id = logged_user_id;       
+                var user_id = logged_user_id;
                 var user_pass_code_of = "user_pass_code_of_" + user_id;
 
-            //looking for pass code in cookies  
+                //looking for pass code in cookies
                 AsyncStorage.getItem(user_pass_code_of).then((val) => {
-                    if( val != null ) {
+                    if (val != null) {
                         console.log("pass code verified from cookie");
                         setShowIndicator(false); //hiding loading animation
 
-                        if( val === enteredPassCode ) {
-                        //redirecting to the home page
+                        if (val === enteredPassCode) {
+                            //redirecting to the home page
                             var toCarry = {};
                             toCarry['logged_user_id'] = user_id;
-                            
+
                             Actions.homePage({ toCarry: toCarry });
                         } else {
                             setError("wrong pass code");
@@ -50,25 +49,25 @@ export default function PassCode({toCarry})
                     } else {
                         console.log("pass code verified from api");
 
-                    //sending rqst to api for verifiying pass code
+                        //sending rqst to api for verifiying pass code
                         const api_end_point = api_url_address + "verifyPassCode.php";
-                        axios.post( api_end_point, {
+                        axios.post(api_end_point, {
                             user_id: user_id,
                             passcode: enteredPassCode,
                         }).then(function(response) {
                             setShowIndicator(false); //hiding loading animation
 
                             var data = (response.data).toString();
-                            if(data == 0) {
+                            if (data == 0) {
                                 setError("wrong pass code");
-                            } else if( data == 1) {
-                            //creating cookie  
+                            } else if (data == 1) {
+                                //creating cookie
                                 AsyncStorage.setItem(user_pass_code_of, enteredPassCode);
 
-                            //redirecting to the home page
+                                //redirecting to the home page
                                 var toCarry = {};
                                 toCarry['logged_user_id'] = user_id;
-                                
+
                                 Actions.homePage({ toCarry: toCarry });
                             } else {
                                 setError("something went wrong");
@@ -77,7 +76,7 @@ export default function PassCode({toCarry})
                             setShowIndicator(false); //hiding loading animation
                             toast("please check your internet connection");
                         });
-                    }  
+                    }
                 });
             }
         } else {
@@ -85,49 +84,47 @@ export default function PassCode({toCarry})
             setError("");
         }
     }, [enteredPassCode]);
-  
-//rendering
-  return (
-    <View style={globalStyles.container}>
-        <Header toCarry={ {title: "MNgo Notes"} } />
-        {
-            showIndicator ?
-            <ActivityIndicator size="large" color="#d8d8d8" />
-            : null
-        }
 
-        <View style={styles.loginContainer}>
-            <Text style={styles.logoText}>Enter Your Pass Code</Text> 
-            <TextInput style={[ globalStyles.inputBox, styles.inputBox ]}
-                placeholder="Pass Code"
-                placeholderTextColor = "#d8d8d8"
-                selectionColor="#1c313a"
-                secureTextEntry={true}
-                autoFocus
-                keyboardType="number-pad"
-                maxLength={ 4 }
-                onChangeText={(val) => setEnteredPassCode(val)}
-            />
+    //rendering
+    return (
+        <View style={globalStyles.container}>
+            <Header toCarry={{ title: "MNgo Notes" }} />
+            {
+                showIndicator ?
+                    <ActivityIndicator size="large" color="#d8d8d8" />
+                    : null
+            }
 
-            <Text style={globalStyles.errorText} >{error}</Text>
-      </View>
-    </View>
-  );
+            <View style={styles.loginContainer}>
+                <Text style={styles.logoText}>Enter Your Pass Code</Text>
+                <TextInput style={[globalStyles.inputBox, styles.inputBox]}
+                    placeholder="Pass Code"
+                    placeholderTextColor="#d8d8d8"
+                    selectionColor="#1c313a"
+                    secureTextEntry={true}
+                    autoFocus
+                    keyboardType="number-pad"
+                    maxLength={4}
+                    onChangeText={(val) => setEnteredPassCode(val)}
+                />
+
+                <Text style={globalStyles.errorText} >{error}</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-    loginContainer:
-    {
-      flex: 1,
-      alignItems:'center',
-      justifyContent :'center',
-      width: '100%',
+    loginContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
     },
 
-    logoText :
-    {
+    logoText: {
         fontSize: 15,
-        color:'#fff'
+        color: '#fff'
     },
 
     inputBox: {
